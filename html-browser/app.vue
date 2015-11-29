5f9c2f8321cc305c91e46958ad5af4645ed3944a
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2015-11-29 13:18:41 -0200

    Using datatables

--- html-browser/app.vue
+++ html-browser/app.vue
@@ -2,20 +2,23 @@
 	<form style="margin-bottom: 30px">
 		<fieldset>
 			<legend>Filters</legend>
-			
+
 			<b-row>
-				<b-select2 label="Authors" colspan="4" multiple allow-clear empty-text="All" :options="authors" :format="formatAuthor" :selected.sync="selectedAuthors"></b-select2>
+				<b-select2 label="Authors" colspan="4" multiple allow-clear empty-text="All" :options="authors" :format="formatAuthor"
+						   :selected.sync="selectedAuthors"></b-select2>
 				<b-select2 label="Languages" colspan="4" multiple allow-clear empty-text="All" :options="languages" :format="formatLanguage"
 						   :selected.sync="selectedLanguages"></b-select2>
-				<b-select2 label="First month" colspan="2" allow-clear empty-text="All" :options="months" :selected.sync="firstMonth"></b-select2>
-				<b-select2 label="Last month" colspan="2" allow-clear empty-text="All" :options="months" :selected.sync="lastMonth"></b-select2>
+				<b-select2 label="First month" colspan="2" allow-clear empty-text="All" :options="months"
+						   :selected.sync="firstMonth"></b-select2>
+				<b-select2 label="Last month" colspan="2" allow-clear empty-text="All" :options="months"
+						   :selected.sync="lastMonth"></b-select2>
 			</b-row>
 		</fieldset>
 	</form>
-	
+
 	<b-tabs>
 		<b-tab title="Authors">
-			<b-grid page-size="10" bordered="true" striped="true" hover="true" condensed="true" colspan="12" :model="tableAuthors">
+			<b-datatable colspan="12" :model="tableAuthors">
 				<b-column header="Author" width="20%" :render="renderName"></b-column>
 				<b-column :header="['Lines', 'Total']" field="lines.total" class="text-right"></b-column>
 				<b-column :header="['Lines', '%']" field="lines.percent" class="text-right"></b-column>
@@ -27,10 +30,10 @@
 				<b-column :header="['Commits', 'Total']" field="commits.total" class="text-right"></b-column>
 				<b-column :header="['Commits', 'First']" field="commits.start" class="text-right"></b-column>
 				<b-column :header="['Commits', 'Last']" field="commits.end" class="text-right"></b-column>
-			</b-grid>
+			</b-datatable>
 		</b-tab>
 		<b-tab title="Months">
-			<b-grid page-size="10" bordered="true" striped="true" hover="true" condensed="true" colspan="12" :model="tableMonths">
+			<b-datatable colspan="12" :model="tableMonths">
 				<b-column header="Month" width="20%" :render="renderName"></b-column>
 				<b-column :header="['Lines', 'Total']" field="lines.total" class="text-right"></b-column>
 				<b-column :header="['Lines', '%']" field="lines.percent" class="text-right"></b-column>
@@ -41,10 +44,10 @@
 				<b-column header="Languages" field="languages" class="text-right"></b-column>
 				<b-column header="Commits" field="commits.total" class="text-right"></b-column>
 				<b-column header="Authors" field="authors" class="text-right"></b-column>
-			</b-grid>
+			</b-datatable>
 		</b-tab>
 		<b-tab title="Languages">
-			<b-grid page-size="10" bordered="true" striped="true" hover="true" condensed="true" colspan="12" :model="tableLanguages">
+			<b-datatable colspan="12" :model="tableLanguages">
 				<b-column header="Language" width="20%" :render="renderName"></b-column>
 				<b-column :header="['Lines', 'Total']" field="lines.total" class="text-right"></b-column>
 				<b-column :header="['Lines', '%']" field="lines.percent" class="text-right"></b-column>
@@ -56,10 +59,10 @@
 				<b-column :header="['Commits', 'First']" field="commits.start" class="text-right"></b-column>
 				<b-column :header="['Commits', 'Last']" field="commits.end" class="text-right"></b-column>
 				<b-column header="Authors" field="authors" class="text-right"></b-column>
-			</b-grid>
+			</b-datatable>
 		</b-tab>
 		<b-tab title="Files">
-			<b-grid page-size="10" bordered="true" striped="true" hover="true" condensed="true" colspan="12" :model="tableFiles">
+			<b-datatable colspan="12" :model="tableFiles">
 				<b-column header="File" width="20%" :render="renderName"></b-column>
 				<b-column :header="['Lines', 'Total']" field="lines.total" class="text-right"></b-column>
 				<b-column :header="['Lines', '%']" field="lines.percent" class="text-right"></b-column>
@@ -70,241 +73,224 @@
 				<b-column :header="['Commits', 'First']" field="commits.start" class="text-right"></b-column>
 				<b-column :header="['Commits', 'Last']" field="commits.end" class="text-right"></b-column>
 				<b-column header="Authors" field="authors" class="text-right"></b-column>
-			</b-grid>
+			</b-datatable>
 		</b-tab>
 	</b-tabs>
 </template>
 
 <script>
 	require('./bootstrap-vue/bootstrap-vue.js')
-	
+
 	var COL_LANGUAGE = 0;
 	var COL_LINE_TYPE = 1;
 	var COL_MONTH = 2;
 	var COL_COMMIT = 3;
 	var COL_AUTHOR = 4;
 	var COL_FILE = 5;
-	
+
 	var EMPTY = "Empty";
 	var CODE = "Code";
 	var COMMENT = "Comment";
-	
+
 	var db = {
-		all: require("./data.js")
-				.create(),
+		all: require("./data.js").create(),
 		selectedAuthors: [],
 		selectedLanguages: [],
 		firstMonth: null,
 		lastMonth: null
 	};
-	
+
 	var totalLines = db.all.sum();
-	
-	$(document)
-			.ready(function () {
-				$('select')
-						.select2({
-							placeholder: "All",
-							allowClear: true
-						});
-			});
-	
+
+	$(document).ready(function () {
+		$('select').select2({
+			placeholder: "All",
+			allowClear: true
+		});
+
+		$(document).on('mouseenter', 'th,td', function () {
+			var $el = $(this);
+			var title = $el.attr('title');
+			if (!title) {
+				if (this.offsetWidth < this.scrollWidth)
+					$el.attr('title', $el.text());
+			} else {
+				if (this.offsetWidth >= this.scrollWidth && title == $el.text())
+					$el.removeAttr('title');
+			}
+		});
+	});
+
 	var palette = ["#4bb2c5", "#c5b47f", "#EAA228", "#579575", "#839557", "#958c12", "#953579", "#4b5de4", "#d8b83f", "#ff5800", "#0085cc"];
-	
+
 	function formatAuthor(a) {
 		return a || '<Umblamable>';
 	}
-	
+
 	function formatLanguage(a) {
 		return a || '<Unknown>';
 	}
-	
+
 	function createAuthorsTab(data) {
 		var table = createTable(data, COL_AUTHOR);
-		
+
 		createFullGrid('authors-table', table, {authors: false});
 		createDonuts('authors', table);
 		createPerMonthGraphs("authors", table)
 	}
-	
+
 	function createMonthsTab(data) {
 		var table = createTable(data, COL_MONTH);
 		removeColors(table);
-		
+
 		createFullGrid('months-table', table, {months: false});
 		createPerMonthGraph('months-graph', data);
 	}
-	
+
 	function createLanguagesTab(data) {
 		var table = createTable(data, COL_LANGUAGE);
-		
+
 		createFullGrid('langs-table', table, {languages: false});
 		createDonuts('langs', table);
 		createPerMonthGraphs("langs", table)
 	}
-	
+
 	function createFilesTab(data) {
 		var table = createTable(data, COL_FILE);
 		removeColors(table);
-		
-		createFullGrid('files-table', table, {
-			files: false,
-			languages: false
-		});
+
+		createFullGrid('files-table', table, {files: false, languages: false});
 	}
-	
+
 	function removeColors(table) {
 		for (var i = 0; i < table.length; ++i)
 			delete table[i].color;
 	}
-	
+
 	function createTable(data, col) {
 		var result = [];
 		var i;
-		
+
 		var items = data.getDistinct(col);
 		for (i = 0; i < items.length; ++i) {
 			var item = items[i];
-			
+
 			var itemData = data.filter(col, item);
-			
+
 			var mr = getFirstAndLastMonths(itemData);
-			
+
 			var itemLines = itemData.sum();
-			
+
 			result.push({
 				data: itemData,
 				name: formatAuthor(item),
 				lines: {
 					total: itemLines,
 					percent: percent(itemLines, totalLines),
-					code: itemData.filter(COL_LINE_TYPE, CODE)
-								  .sum(),
-					comment: itemData.filter(COL_LINE_TYPE, COMMENT)
-									 .sum(),
-					empty: itemData.filter(COL_LINE_TYPE, EMPTY)
-								   .sum()
+					code: itemData.filter(COL_LINE_TYPE, CODE).sum(),
+					comment: itemData.filter(COL_LINE_TYPE, COMMENT).sum(),
+					empty: itemData.filter(COL_LINE_TYPE, EMPTY).sum()
 				},
-				files: itemData.getDistinct(COL_FILE)
-							   .count(),
-				languages: itemData.getDistinct(COL_LANGUAGE)
-								   .count(),
+				files: itemData.getDistinct(COL_FILE).count(),
+				languages: itemData.getDistinct(COL_LANGUAGE).count(),
 				commits: {
-					total: itemData.getDistinct(COL_COMMIT)
-								   .count(),
+					total: itemData.getDistinct(COL_COMMIT).count(),
 					start: mr[0],
 					end: mr[1]
 				},
-				authors: itemData.getDistinct(COL_AUTHOR)
-								 .count()
+				authors: itemData.getDistinct(COL_AUTHOR).count()
 			});
 		}
-		
+
 		result = result.sortBy(function (a) {
 			return a.lines.total;
 		}, true);
-		
+
 		for (i = 0; i < result.length; ++i) {
 			if (result[i].name === "<Unblamable>")
-				result[i].color = "#D1DDDF"; else
+				result[i].color = "#D1DDDF";
+			else
 				result[i].color = palette[i % palette.length];
 		}
-		
+
 		return result;
 	}
-	
+
 	function getFirstAndLastMonths(data) {
 		var result = data.getDistinct(COL_MONTH)
-						 .exclude("");
+				.exclude("");
 		result.sort();
-		
+
 		if (result.length < 1)
-			return ["unknown", "unknown"]; else
+			return ["unknown", "unknown"];
+		else
 			return [result[0], result[result.length - 1]];
 	}
-	
+
 	function getMonthRange(data) {
 		var fl = getFirstAndLastMonths(data);
 		if (fl[0] === "unknown")
 			return [];
-		
+
 		return Date.range(fl[0], fl[1])
-				   .every('month')
-				   .map(function (m) {
-					   return m.format('{yyyy}-{MM}');
-				   });
+				.every('month')
+				.map(function (m) {
+					return m.format('{yyyy}-{MM}');
+				});
 	}
-	
+
 	function percent(count, total) {
 		return parseInt(count * 1000 / total) / 10;
 	}
-	
+
 	function createFullGrid(div, table, opts) {
-		var cols = [{
-			data: null,
-			render: function (data, type, row) {
-				if (type === 'display') {
-					if (row.color)
-						return "<span style='background-color: " + row.color + "; width: 15px; height: 15px; float: left; border: 1px solid #aaa; margin-right: 4px;'></span> " + htmlEscape(row.name); else
-						return htmlEscape(row.name);
+		var cols = [
+			{
+				data: null,
+				render: function (data, type, row) {
+					if (type === 'display') {
+						if (row.color)
+							return "<span style='background-color: " + row.color
+									+ "; width: 15px; height: 15px; float: left; border: 1px solid #aaa; margin-right: 4px;'></span> "
+									+ htmlEscape(row.name);
+						else
+							return htmlEscape(row.name);
+					}
+
+					return row.name;
 				}
-				
-				return row.name;
-			}
-		}, {
-			data: 'lines.total',
-			className: 'text-right'
-		}, {
-			data: 'lines.percent',
-			className: 'text-right'
-		}, {
-			data: 'lines.code',
-			className: 'text-right'
-		}, {
-			data: 'lines.comment',
-			className: 'text-right'
-		}, {
-			data: 'lines.empty',
-			className: 'text-right'
-		}];
-		
+			},
+			{data: 'lines.total', className: 'text-right'},
+			{data: 'lines.percent', className: 'text-right'},
+			{data: 'lines.code', className: 'text-right'},
+			{data: 'lines.comment', className: 'text-right'},
+			{data: 'lines.empty', className: 'text-right'}
+		];
+
 		if (opts.files !== false)
-			cols.push({
-				data: 'files',
-				className: 'text-right'
-			});
-		
+			cols.push({data: 'files', className: 'text-right'});
+
 		if (opts.languages !== false)
-			cols.push({
-				data: 'languages',
-				className: 'text-right'
-			});
-		
-		cols.push({
-			data: 'commits.total',
-			className: 'text-right'
-		});
-		
+			cols.push({data: 'languages', className: 'text-right'});
+
+		cols.push({data: 'commits.total', className: 'text-right'});
+
 		if (opts.months !== false) {
 			cols.push({data: 'commits.start'});
 			cols.push({data: 'commits.end'});
 		}
-		
+
 		if (opts.authors !== false)
-			cols.push({
-				data: 'authors',
-				className: 'text-right'
-			});
-		
-		$('#' + div)
-				.DataTable({
-					destroy: true,
-					data: table,
-					order: [[1, "desc"]],
-					columns: cols
-				});
+			cols.push({data: 'authors', className: 'text-right'});
+
+		$('#' + div).DataTable({
+			destroy: true,
+			data: table,
+			order: [[1, "desc"]],
+			columns: cols
+		});
 	}
-	
+
 	function htmlEscape(str) {
 		return String(str)
 				.replace(/&/g, '&amp;')
@@ -313,27 +299,25 @@
 				.replace(/</g, '&lt;')
 				.replace(/>/g, '&gt;');
 	}
-	
+
 	function createDonuts(div, table) {
 		var itens = ["Total", "Code", "Comment", "Empty"];
-		
+
 		for (var i = 0; i < itens.length; ++i) {
 			var id = div + "-donut-" + i;
-			
-			$("#" + div)
-					.append("<div class='col-md-3'><div id='" + id + "' style='width: 100%; height: 300px'></div></div>");
-			
+
+			$("#" + div).append("<div class='col-md-3'><div id='" + id + "' style='width: 100%; height: 300px'></div></div>");
+
 			createDonut(id, table, itens[i].toLowerCase(), "Lines: " + itens[i]);
 		}
 	}
-	
+
 	function createDonut(div, table, field, title) {
 		var dounutData = table.map(function (a) {
 			return [a.name, a.lines[field]];
 		});
-		
-		$("#" + div)
-				.empty();
+
+		$("#" + div).empty();
 		$.jqplot(div, [dounutData], {
 			title: title,
 			seriesDefaults: {
@@ -354,26 +338,26 @@
 			})
 		});
 	}
-	
+
 	function createPerMonthGraphs(div, table) {
 		for (var i = 0; i < table.length; ++i) {
 			var item = table[i];
 			var id = div + "-month-" + i;
-			
-			$("#" + div)
-					.append("<div class='col-md-12'>" + "<div id='" + id + "' style='width: 100%; height: 200px'></div>" + "</div>");
-			
+
+			$("#" + div).append("<div class='col-md-12'>" +
+					"<div id='" + id + "' style='width: 100%; height: 200px'></div>" +
+					"</div>");
+
 			createPerMonthGraph(id, item.data, item.name);
 		}
 	}
-	
+
 	function createPerMonthGraph(div, data, title) {
 		var code = prepareForGraphPerMonth(data.filter(COL_LINE_TYPE, CODE));
 		var comments = prepareForGraphPerMonth(data.filter(COL_LINE_TYPE, COMMENT));
 		var empty = prepareForGraphPerMonth(data.filter(COL_LINE_TYPE, EMPTY));
-		
-		$("#" + div)
-				.empty();
+
+		$("#" + div).empty();
 		$.jqplot(div, [code, comments, empty], {
 			title: title,
 			axes: {
@@ -398,7 +382,11 @@
 				background: "transparent",
 				borderColor: "transparent"
 			},
-			series: [{label: 'Code'}, {label: 'Comments'}, {label: 'Empty lines'}],
+			series: [
+				{label: 'Code'},
+				{label: 'Comments'},
+				{label: 'Empty lines'}
+			],
 			legend: {
 				show: true,
 				placement: 'outsideGrid'
@@ -411,23 +399,23 @@
 			seriesColors: ["#4BB2C5", "#ADC8A7", "#F8EED5"]
 		});
 	}
-	
+
 	function prepareForGraphPerMonth(data) {
 		data = data.groupBy(COL_MONTH);
-		
+
 		months.forEach(function (m) {
 			data.inc(0, m);
 		});
-		
+
 		return data.map(function (val, month) {
-					   return [Date.create(month), val];
-				   })
-				   .sortBy(function (i) {
-					   return i[0]
-				   });
-		
+					return [Date.create(month), val];
+				})
+				.sortBy(function (i) {
+					return i[0]
+				});
+
 	}
-	
+
 	module.exports = {
 		data: function () {
 			return db;
@@ -435,11 +423,11 @@
 		computed: {
 			authors: function () {
 				return this.all.getDistinct(COL_AUTHOR)
-						   .sortBy();
+						.sortBy();
 			},
 			languages: function () {
 				return this.all.getDistinct(COL_LANGUAGE)
-						   .sortBy();
+						.sortBy();
 			},
 			months: function () {
 				return getMonthRange(this.all);
@@ -447,27 +435,27 @@
 			db: function () {
 				var self = this;
 				var result = self.all;
-				
+
 				if (self.selectedAuthors.length)
 					result = result.filter(COL_AUTHOR, function (a) {
 						return self.selectedAuthors.any(a);
 					});
-				
+
 				if (self.selectedLanguages.length)
 					result = result.filter(COL_LANGUAGE, function (a) {
 						return self.selectedLanguages.any(a);
 					});
-				
+
 				if (self.firstMonth)
 					result = result.filter(COL_MONTH, function (a) {
 						return a >= self.firstMonth;
 					});
-				
+
 				if (self.lastMonth)
 					result = result.filter(COL_MONTH, function (a) {
 						return a <= self.lastMonth;
 					});
-				
+
 				return result;
 			},
 			tableAuthors: function () {
--- html-browser/bootstrap-vue/b-datatable.vue
+++ html-browser/bootstrap-vue/b-datatable.vue
@@ -1,3 +1,23 @@
+<style scoped>
+	table {
+		table-layout: fixed;
+	}
+	
+	th {
+		text-align: center;
+		vertical-align: top !important;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+	
+	td {
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+</style>
+
 <template>
 	<b-col-sm :cols="colspan">
 		<table v-el:table class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">
@@ -12,9 +32,9 @@
 </template>
 
 <script>
-
+	
 	var Vue = require('vue')
-
+	
 	module.exports = {
 		mixins: [require('./mixin-colspan.js')],
 		data: function () {
@@ -27,56 +47,57 @@
 		},
 		methods: {
 			_registerColumn: function (col) {
-				if (!this.internal_columns)
+				if (!this.internal_columns) {
 					this.internal_columns = [];
-
+				}
+				
 				this.internal_columns.push(col);
 			},
 			_computeHeaders: function () {
 				if (!this.internal_columns)
 					this.internal_columns = [];
-
+				
 				var result = [];
-
+				
 				var maxDepth = 1;
 				for (var i = 0; i < this.internal_columns.length; ++i) {
 					var col = this.internal_columns[i];
 					if (typeof col.header != 'string')
 						maxDepth = Math.max(maxDepth, col.header.length);
 				}
-
+				
 				for (var i = 0; i < this.internal_columns.length; ++i) {
 					var col = this.internal_columns[i];
-
+					
 					var titles = col.header;
 					if (typeof titles == 'string')
 						titles = [titles];
-
+					
 					var insideNew = false;
-
+					
 					for (var j = 0; j < titles.length; ++j) {
 						var title = titles[j];
-
+						
 						if (result.length <= j)
 							result[j] = [];
-
+						
 						var headerLine = result[j];
 						var header = headerLine[headerLine.length - 1] || {};
-
+						
 						if (insideNew || header.title !== title) {
 							header = {
 								rowspan: 1,
 								colspan: 0,
 								title: title
 							};
-
+							
 							headerLine.push(header);
-
+							
 							insideNew = true;
 						}
-
+						
 						header.colspan++;
-
+						
 						if (j == titles.length - 1) {
 							if (col.width)
 								header.width = col.width;
@@ -84,47 +105,47 @@
 						}
 					}
 				}
-
+				
 				return result;
 			},
 			_createColumnConfig: function (col) {
 				var result = {
 					data: col.field || null
 				};
-
+				
 				if (col.class)
 					result.className = col.class;
-
+				
 				if (col.render) {
 					result.render = function (data, type, row) {
 						if (type === 'display')
-							return col.render(row);
-						else
+							return col.render(row); else
 							return row.name;
 					};
 				}
-
+				
 				return result;
 			},
 			_updateDataTable: function () {
 				var self = this;
-
+				
 				Vue.nextTick(function () {
 					var cols = [];
 					for (var i = 0; i < self.internal_columns.length; ++i) {
 						var col = self.internal_columns[i];
-
+						
 						cols.push(self._createColumnConfig(col));
 					}
-
-					self.internal_datatable = $(self.$els.table).DataTable({
-						destroy: true,
-						searching: false,
-						lengthChange: false,
-						autoWidth: false,
-						data: self.model,
-						columns: cols
-					});
+					
+					self.internal_datatable = $(self.$els.table)
+							.DataTable({
+								destroy: true,
+								searching: false,
+								lengthChange: false,
+								autoWidth: false,
+								data: self.model,
+								columns: cols
+							});
 				})
 			},
 		},
